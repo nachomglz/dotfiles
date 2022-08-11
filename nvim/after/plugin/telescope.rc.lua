1f73a71b54d552561984e752b3fc04fbce9003bb
nachomglz <nachopad3@gmail.com>
2022-08-11 14:24:52 +0200

    Modified fish and tmux config files

--- nvim/after/plugin/telescope.rc.lua
+++ nvim/after/plugin/telescope.rc.lua
@@ -1,42 +1,46 @@
-local status, telescope = pcall(require, 'telescope')
+local status, telescope = pcall(require, "telescope")
 if (not status) then return end
 local actions = require('telescope.actions')
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
 local function telescope_buffer_dir()
   return vim.fn.expand('%:p:h')
 end
 
-local fb_actions = require 'telescope'.extensions.file_browser.actions
+local fb_actions = require "telescope".extensions.file_browser.actions
 
 telescope.setup {
   defaults = {
     mappings = {
       n = {
-        ['q'] = actions.close
-      }
-    }
+        ["q"] = actions.close
+      },
+    },
   },
   extensions = {
     file_browser = {
-      theme = 'dropdown',
-      --disables netwr add use telescope-file-browser in its place
+      theme = "dropdown",
+      -- disables netrw and use telescope-file-browser in its place
       hijack_netrw = true,
       mappings = {
+        -- your custom insert mode mappings
+        ["i"] = {
+          ["<C-w>"] = function() vim.cmd('normal vbd') end,
+        },
         ["n"] = {
-          -- custom normal mode mappings
+          -- your custom normal mode mappings
           ["N"] = fb_actions.create,
           ["h"] = fb_actions.goto_parent_dir,
           ["/"] = function()
             vim.cmd('startinsert')
           end
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 }
 
-telescope.load_extension('file_browser')
+telescope.load_extension("file_browser")
 
 vim.keymap.set('n', ';f',
   function()
@@ -72,41 +76,3 @@ vim.keymap.set("n", "sf", function()
     layout_config = { height = 40 }
   })
 end)
-if (not status) then return end
-local actions = require('telescope.actions')
-local builtin = require('telescope.builtin')
-
-local function telescope_buffer_dir()
-  return vim.fn.expand('%:p:h')
-end
-
-local fb_actions = require 'telescope'.extensions.file_browser.actions
-
-telescope.setup {
-  defaults = {
-    mappings = {
-      n = {
-        ['q'] = actions.close
-      }
-    }
-  },
-  extensions = {
-    file_browser = {
-      theme = 'dropdown',
-      --disables netwr add use telescope-file-browser in its place
-      hijack_netrw = true,
-      mappings = {
-      }
-    }
-  }
-}
-
-telescope.load_extension('file_browser')
-
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', ';f', '<cmd>lua require("telescope.builtin").find_files({ no_ignore = false, hidden = true })<cr>',
-  opts)
-vim.keymap.set('n', ';r', '<cmd>lua require("telescope.builtin").live_grep()<cr>',
-  opts)
-vim.keymap.set('n', '\\\\', '<cmd>lua require("telescope.builtin").buffers()<cr>',
-  opts)
