f3e7b846e8454bc94f81a6c8961f60b53e8731d8
Nacho Martin <72929135+nachomglz@users.noreply.github.com>
2024-09-29 14:52:14 +0200

    chore: updated tmux and tmux integrations with neovim

--- nvim/lua/nacho/core/keymaps.lua
+++ nvim/lua/nacho/core/keymaps.lua
@@ -15,11 +15,27 @@ keymap.set("n", "<leader>ss", "<C-w>s", { desc = "Horizontal split" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make vertical windows the same size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
--- Split window pane management
-keymap.set("n", "<leader>h", "<C-w>h", { noremap = true, desc = "Move to left pane" })
-keymap.set("n", "<leader>j", "<C-w>j", { noremap = true, desc = "Move to below pane" })
-keymap.set("n", "<leader>k", "<C-w>k", { noremap = true, desc = "Move to above pane" })
-keymap.set("n", "<leader>l", "<C-w>l", { noremap = true, desc = "Move to right pane" })
+-- Resize panes (with tmux plugin)
+-- keymap.del({ "n", "i", "v" }, "<A-j>")
+-- keymap.del({ "n", "i", "v" }, "<A-k>")
+-- keymap.del("n", "<C-Left>")
+-- keymap.del("n", "<C-Down>")
+-- keymap.del("n", "<C-Up>")
+-- keymap.del("n", "<C-Right>")
+
+local resize_opts = { noremap = true, silent = true }
+
+keymap.set("n", "<M-h>", '<cmd>lua require("tmux").resize_left()<cr>', resize_opts)
+keymap.set("n", "<M-j>", '<cmd>lua require("tmux").resize_bottom()<cr>', resize_opts)
+keymap.set("n", "<M-k>", '<cmd>lua require("tmux").resize_top()<cr>', resize_opts)
+keymap.set("n", "<M-l>", '<cmd>lua require("tmux").resize_right()<cr>', resize_opts)
+--
+-- Tmux/Vim Nagivation
+keymap.set("n", "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>", resize_opts)
+keymap.set("n", "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>", resize_opts)
+keymap.set("n", "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>", resize_opts)
+keymap.set("n", "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>", resize_opts)
+keymap.set("n", "<C-\\>", "<cmd>NvimTmuxNavigateLastActive<cr>", resize_opts)
 
 -- tab management
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "open new tab" })
