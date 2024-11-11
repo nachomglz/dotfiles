7a3fff8a0a4785f067d0ea8f938c316d5882d868
Nacho Martin <72929135+nachomglz@users.noreply.github.com>
2024-11-11 12:40:33 +0100

    feat: search for TODO comments using telescope or trouble

---
+++ nvim/lua/nacho/plugins/todo-comments.lua
@@ -0,0 +1,13 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local keymap = vim.keymap
+		local todo_comments = require("todo-comments")
+
+		todo_comments.setup()
+
+		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find all TODO's using Telescope" })
+		keymap.set("n", "<leader>xt", "<cmd>TodoTrouble<cr>", { desc = "Find all TODO's using Trouble" })
+	end,
+}
