655df2a8b27d7ee2a4fb6e40d22d569cf08c9246
Nacho Martin <72929135+nachomglz@users.noreply.github.com>
2024-12-18 11:47:09 +0100

    chore: disable background color for todo comments

--- nvim/lua/nacho/plugins/todo-comments.lua
+++ nvim/lua/nacho/plugins/todo-comments.lua
@@ -5,7 +5,11 @@ return {
 		local keymap = vim.keymap
 		local todo_comments = require("todo-comments")
 
-		todo_comments.setup()
+		todo_comments.setup({
+			highlight = {
+				keyword = "fg",
+			},
+		})
 
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find all TODO's using Telescope" })
 		keymap.set("n", "<leader>xt", "<cmd>TodoTrouble<cr>", { desc = "Find all TODO's using Trouble" })
